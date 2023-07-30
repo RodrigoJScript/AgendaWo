59473a6c8730719462fc0b7c35ff2bb6a76a0d2e
RodrigoJScript <rodrigo777.rjp@gmail.com>
2023-07-29 23:08:29 -0600

    huella digital

--- app/src/main/java/com/rodrigojscript/agendawow/MainActivity.kt
+++ app/src/main/java/com/rodrigojscript/agendawow/MainActivity.kt
@@ -1,34 +1,35 @@
 package com.rodrigojscript.agendawow
 
-import android.Manifest
-import android.content.Intent
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.core.content.FileProvider
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.rodrigojscript.agendawow.ui.screens.GuardarDatos
-import com.rodrigojscript.agendawow.ui.screens.Inicio
 import com.rodrigojscript.agendawow.ui.screens.MostrarDatos
 import com.rodrigojscript.agendawow.ui.theme.AgendaWowTheme
 import com.rodrigojscript.agendawow.viewModel.AgendaViewModel
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val agendaViewModel: AgendaViewModel = ViewModelProvider(this)[AgendaViewModel::class.java]
@@ -36,8 +37,7 @@ class MainActivity : ComponentActivity() {
             AgendaWowTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
                     NavHost(navController = navController, startDestination = "inicio") {
@@ -45,13 +45,70 @@ class MainActivity : ComponentActivity() {
                         composable("mostrarDatos") { MostrarDatos(navController, agendaViewModel) }
                         composable("guardarDatos") {
                             GuardarDatos(
-                                agendaViewModel = agendaViewModel,
-                                navController
+                                agendaViewModel = agendaViewModel, navController
                             )
                         }
                     }
                 }
             }
         }
+        setupAuth()
     }
+
+    private var canAuth = false
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+    private fun setupAuth() {
+        if (BiometricManager.from(this)
+                .canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL) == BiometricManager.BIOMETRIC_SUCCESS
+        ) {
+            canAuth = true
+            promptInfo = BiometricPrompt.PromptInfo.Builder().setTitle("Autenticacion biometrica")
+                .setSubtitle("Autenticate utilizando el sensor biometrico")
+                .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                .build()
+        }
+    }
+
+    private fun authenticate(auth: (auth: Boolean) -> Unit) {
+        if (canAuth) {
+            BiometricPrompt(
+                this,
+                ContextCompat.getMainExecutor(this),
+                object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+
+                        auth(true)
+                    }
+                }).authenticate(promptInfo)
+        } else {
+            auth(true)
+        }
+    }
+
+    @Composable
+    fun Inicio(navController: NavHostController) {
+        var auth by rememberSaveable { mutableStateOf(false) }
+        Column {
+            Button(onClick = { navController.navigate("guardarDatos") }, enabled = auth) {
+                Text(text = "Guardar datos")
+            }
+            Button(onClick = { navController.navigate("mostrarDatos") }, enabled = auth) {
+                Text(text = "Mostrar datos")
+            }
+            Button(onClick = {
+                authenticate {
+                    if (auth) {
+                        auth = false
+                    } else {
+                        auth = it
+                    }
+                }
+            }) {
+                Text(text = "Autenticar")
+            }
+        }
+    }
+
 }
+
--- app/src/main/java/com/rodrigojscript/agendawow/ui/screens/Inicio.kt
+++ app/src/main/java/com/rodrigojscript/agendawow/ui/screens/Inicio.kt
@@ -4,19 +4,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
-@Composable
-fun Inicio(navController: NavHostController) {
-    Column {
-        Button(onClick = { navController.navigate("guardarDatos") }) {
-            Text(text = "Guardar datos")
-        }
-        Button(onClick = { navController.navigate("mostrarDatos") }) {
-            Text(text = "Mostrar datos")
-        }
-    }
-}
